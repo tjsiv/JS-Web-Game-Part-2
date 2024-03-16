342886a83160e4a6155a06ff688cac2e1d42a48c
tjsiv <tjsivelsjr@gmail.com>
2024-03-16 15:38:32 -0700

    finish with bonus now

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+
+function map(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -9,33 +18,27 @@ function newImage(url, left, bottom){
 }
 
 function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })
 }
 
-function newInventory(){
-    let inventory = document.createElement('div')
-    inventory.style.position = 'fixed'
-    inventory.style.bottom = '0px';
-    inventory.style.left = '0px'
-    inventory.style.width = '100%'
-    inventory.style.height = '100px'
-    inventory.style.display = 'flex'
-    inventory.style.flexDirection = 'row'
-    inventory.style.alignItems = 'center'
-    inventory.style.justifyContent = 'space-evenly'
-    inventory.style.border = '2px solid black'
-    inventory.style.backgroundColor = 'brown'
-    document.body.append(inventory)
-}
+let equator = window.innerHeight / 1.75
+let skyHeight = window.innerHeight-equator
+let floorHeight = equator
 
-newInventory()
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+map('assets/sky.png', 0, equator, window.innerWidth/100, skyHeight/100)
+map('assets/grass.png', 0, 0, window.innerWidth/100, floorHeight/100)
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
